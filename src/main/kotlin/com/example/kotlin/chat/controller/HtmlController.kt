54d844c2bad3be99e9222349b1da40ede8a81ec7
Anton Arhipov <ant.arhipov@gmail.com>
2020-12-08 13:33:12 +0200

    clean unused imports

--- src/main/kotlin/com/example/kotlin/chat/controller/HtmlController.kt
+++ src/main/kotlin/com/example/kotlin/chat/controller/HtmlController.kt
@@ -2,7 +2,6 @@ package com.example.kotlin.chat.controller
 
 import com.example.kotlin.chat.service.MessageService
 import com.example.kotlin.chat.service.MessageVM
-import kotlinx.coroutines.flow.toList
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
