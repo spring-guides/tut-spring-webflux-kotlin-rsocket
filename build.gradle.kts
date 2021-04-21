5f60b9191b47ed87e3836935514e81bb71d6856c
Oleh Dokuka <oleh.dokuka@icloud.com>
2021-04-21 10:12:59 +0300

    updates dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.0"
-	id("io.spring.dependency-management") version "1.0.10.RELEASE"
-	kotlin("jvm") version "1.4.10"
-	kotlin("plugin.spring") version "1.4.10"
+	id("org.springframework.boot") version "2.4.5"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.4.32"
+	kotlin("plugin.spring") version "1.4.32"
 }
 
 group = "com.example.kotlin"
@@ -35,7 +35,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	runtimeOnly("com.h2database:h2")
 
-	implementation("org.jetbrains:markdown:0.1.45")
+	implementation("org.jetbrains:markdown:0.2.2")
 }
 
 tasks.withType<Test> {
