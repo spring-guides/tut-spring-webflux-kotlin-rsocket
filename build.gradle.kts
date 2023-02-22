c0ae89d057f3a3415fc2850a730e0fda48c52006
Andrii Maliuta <quadr988@gmail.com>
2023-02-22 17:30:45 +0200

    Update build.gradle.kts
    indents changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.2"
-    id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.7.22"
-    kotlin("plugin.spring") version "1.7.22"
+        id("org.springframework.boot") version "3.0.2"
+        id("io.spring.dependency-management") version "1.1.0"
+        kotlin("jvm") version "1.7.22"
+        kotlin("plugin.spring") version "1.7.22"
 }
 
 group = "com.example.kotlin"
@@ -13,32 +13,34 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 
 repositories {
-    mavenCentral()
+        mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("org.springframework.boot:spring-boot-starter")
+        implementation("org.springframework.boot:spring-boot-starter-actuator")
+        implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+        implementation("org.springframework.boot:spring-boot-starter-web")
 
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("com.github.javafaker:javafaker:1.0.2")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("com.github.javafaker:javafaker:1.0.2")
 
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	    }
 
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
-    }
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+	    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+	        kotlinOptions {
+		    freeCompilerArgs = listOf("-Xjsr305=strict")
+		    jvmTarget = "17"
+	    }
 }
